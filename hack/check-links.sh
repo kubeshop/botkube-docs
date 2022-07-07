801ef1a81898dec8a6153849e9022773e9102f23
Mateusz Szostok <szostok.mateusz@gmail.com>
2022-07-07 21:08:28 +0200

    Ignore history dir

--- hack/check-links.sh
+++ hack/check-links.sh
@@ -9,11 +9,16 @@ REPO_ROOT_DIR=$(cd "${CURRENT_DIR}/.." && pwd)
 readonly CURRENT_DIR
 readonly REPO_ROOT_DIR
 
+check::install() {
+	npm install -g markdown-link-check
+}
+
 check::links() {
-	find ./content  -type f -regex '.*\.md' -exec markdown-link-check {} -q -c "${REPO_ROOT_DIR}/.mlc.config.json" \;
+	find ./content  -type f -regex '.*\.md'  -not -path "./content/history/*" -exec markdown-link-check {} -q -c "${REPO_ROOT_DIR}/.mlc.config.json" \;
 }
 
 main() {
+  check::install
   check::links
 }
 
