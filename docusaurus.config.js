90db2b87bfd504e9abad03ef64ffd7628e830618
Tomasz Rogalski <tomasz@madebyrogal.com>
2022-08-09 10:29:09 +0200

    Add docusaurus scaffold (#113)
    * Add docusaurus scaffold
    
    * fix CR

---
+++ docusaurus.config.js
@@ -0,0 +1,73 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'BotKube',
+  tagline: 'Messaging bot for monitoring and debugging Kubernetes clusters',
+  url: 'https://botkube.io/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'images/favicon.png',
+  organizationName: 'kubeshop',
+  projectName: 'botkube',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/kubeshop/botkube-docs',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'BotKube',
+        logo: {
+          alt: 'BotKube Logo',
+          src: 'images/botkube.png',
+        },
+        items: [
+          {
+            href: 'https://github.com/kubeshop/botkube',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [],
+        copyright: `Copyright © ${new Date().getFullYear()} Kubeshop, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      colorMode: {
+        respectPrefersColorScheme: true,
+      }
+    }),
+};
+
+module.exports = config;
