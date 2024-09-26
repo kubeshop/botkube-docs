691c13c376b51dc23f47a3bb1abd0a2538798f4b
Pawel Kosiec <pawel.kosiec@gmail.com>
2024-09-26 20:33:10 +0200

    Fix React import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
+import React from "react";
 import clsx from "clsx";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import Heading from "@theme/Heading";
 import { CardTutorial } from "@site/src/components/Card/Tutorial";
-
 import styles from "./index.module.css";
 
 function HomepageHeader() {
